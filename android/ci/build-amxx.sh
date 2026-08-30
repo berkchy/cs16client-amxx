56eeb1f95d5fd1db7ad6738bd71a4f656117c808
nenquen <barank2.3107@gmail.com>
2026-08-31 01:26:58 +0300

    ci: build metamod against vendored fwgs hlsdk instead of empty submodule

--- android/ci/build-amxx.sh
+++ android/ci/build-amxx.sh
@@ -103,7 +103,7 @@ SYSROOT_LIB=$NDK/toolchains/llvm/prebuilt/$HOST-x86_64/sysroot/usr/lib/aarch64-l
 AMXX=$SRC/amxmodx
 HLSDK=$REPO_ROOT/android/hlsdk
 METAMOD=$SRC/metamod-p/metamod
-MMHLSDK=$SRC/metamod-p/hlsdk
+MMHLSDK=$HLSDK
 
 # ---------------------------------------------------------------- flags
 DEFS=(
@@ -216,8 +216,9 @@ PCRE_A="$TMP/pcre-inst/lib/libpcre.a"
 echo "== building metamod (metamod-p, aarch64) =="
 MM_INC=(
   -I"$METAMOD"
-  -I"$MMHLSDK/engine" -I"$MMHLSDK/common" -I"$MMHLSDK/pm_shared" -I"$MMHLSDK/dlls" -I"$MMHLSDK"
-  -I"$HLSDK" -I"$HLSDK/dlls"
+  -I"$MMHLSDK/common" -I"$MMHLSDK/dlls" -I"$MMHLSDK/engine"
+  -I"$MMHLSDK/game_shared" -I"$MMHLSDK/public" -I"$MMHLSDK/pm_shared"
+  -I"$MMHLSDK"
 )
 MM_DEFS=(
   -D__METAMOD_BUILD__
