e745b8c276c8dcde4fc2b9cf00048fdb1cf551cd
pickle <pickle@local>
2026-09-01 16:10:31 +0300

    fix: add ReAPI source patches to build script (std::min + size_t dedup)

--- android/ci/build-amxx.sh
+++ android/ci/build-amxx.sh
@@ -534,6 +534,25 @@ cat > "$REAPI/version/reapi_version.inc" << 'VERINC'
 #define REAPI_VERSION_STR "3.1.0"
 VERINC
 echo "   created reapi_version.inc"
+# Fix unqualified min() call and ULONG/size_t dedup for ARM64
+python3 -c "
+import os
+# hook_callback.h: min() -> std::min()
+p='$REAPI/src/hook_callback.h'
+if os.path.exists(p):
+    d=open(p).read()
+    d=d.replace('args_count = min(arg_count, MAX_HOOKCHAIN_ARGS)', 'args_count = std::min(arg_count, MAX_HOOKCHAIN_ARGS)')
+    open(p,'w').write(d)
+# natives_helper.h: guard operator size_t() when ULONG==size_t
+p='$REAPI/src/natives/natives_helper.h'
+if os.path.exists(p):
+    d=open(p).read()
+    old='\toperator size_t() const\n\t{\n\t\treturn size_t(m_value);\n\t}'
+    new='#if ULONG_MAX != SIZE_MAX\n\toperator size_t() const\n\t{\n\t\treturn size_t(m_value);\n\t}\n#endif'
+    d=d.replace(old,new)
+    open(p,'w').write(d)
+print('   patched reapi sources for ARM64')
+"
 REAPI_BASEFLAGS="-std=c++14 -O2 -fPIC -fpermissive -w \
   -D_LINUX -DLINUX -DNDEBUG -D_GLIBCXX_USE_CXX11_ABI=0 \
   -DHAVE_STRONG_TYPEDEF -D_stricmp=strcasecmp -D_strnicmp=strncasecmp \
